4fac7def4aa8b79b33097401e87affbbb9fb7a82
kartikyadav7 <raokartik7503@gmail.com>
2025-04-02 15:39:49 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ['https://adtask-ai-frontend.vercel.app'],
+    origin: ['https://adtask-ai-frontend.vercel.app','http://localhost:5173',],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }))
